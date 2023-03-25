1a764fee1a57e602154cef30bcdadb818f6246de
TimHal <tim.hallyburton@gmx.de>
2023-03-25 21:19:58 +0100

    fix: clean up open handle

--- src/tests/plugin.spec.ts
+++ src/tests/plugin.spec.ts
@@ -1,23 +1,30 @@
-import express, { application } from "express";
+import express from "express";
+import { Server } from "http";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import payload from "payload";
 import { simpleCollectionSlug } from "./configs/simple/payload-config";
+
+let handle: Server;
 
 describe("AutoI18n Plugin Tests", () => {
   beforeAll(async () => {
     process.env["PAYLOAD_CONFIG_PATH"] =
       "src/tests/configs/simple/payload-config.ts";
 
-    const app = express();
-    app.listen(3000);
     const mongod = await MongoMemoryServer.create();
     const uri = mongod.getUri();
+    const app = express();
+    handle = app.listen(3000);
 
     await payload.init({
       secret: "SECRET",
       express: app,
       mongoURL: uri,
     });
+  });
+
+  afterAll(() => {
+    handle.close();
   });
 
   it("Should translate a simple entity", async () => {
