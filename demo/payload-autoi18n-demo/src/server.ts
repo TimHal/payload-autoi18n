10beeec408c3f12932b48d6ceb668f2c587b0e2c
TimHal <tim.hallyburton@gmx.de>
2023-04-14 16:17:08 +0200

    fix(demo): linting

--- demo/payload-autoi18n-demo/src/server.ts
+++ demo/payload-autoi18n-demo/src/server.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import payload from 'payload';
+import express from "express";
+import payload from "payload";
 
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 
 // Redirect root to Admin panel
-app.get('/', (_, res) => {
-  res.redirect('/admin');
+app.get("/", (_, res) => {
+  res.redirect("/admin");
 });
 
 const start = async () => {
@@ -16,13 +16,13 @@ const start = async () => {
     mongoURL: process.env.MONGODB_URI,
     express: app,
     onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
     },
-  })
+  });
 
   // Add your own express routes here
 
   app.listen(3000);
-}
+};
 
 start();
