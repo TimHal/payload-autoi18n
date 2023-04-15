68e2f6b3f0cae496e03db64a83c3e733815de163
TimHal <tim.hallyburton@gmx.de>
2023-04-15 13:34:00 +0200

    feat: demo translate button

--- demo/payload-autoi18n-demo/src/components/translate.component.tsx
+++ demo/payload-autoi18n-demo/src/components/translate.component.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import {useDocumentInfo, useLocale} from 'payload/components/utilities'
+import payload from "payload";
 
 const translateComponent: React.FC = () => {
-    return <div>Hi!</div>
+
+    const { slug, id } = useDocumentInfo();
+    const locale = useLocale();
+    const baseUrl = 'http://localhost:3000/api';
+    const translateFn = e => {
+        console.log('foo')
+        fetch(baseUrl + `/${slug}/${id}/translate?id=${id}&locale=${locale}`, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              method: "POST",
+        }).then( () => location.reload() ) // not the nicest way, but it is sufficient for a demo
+    }
+
+    return <a href="#" onClick={translateFn}>Translate</a>
 }
 
 export default translateComponent;
